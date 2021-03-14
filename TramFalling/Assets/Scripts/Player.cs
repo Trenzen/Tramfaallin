69d98fc14b59f6e933bcfb761a998aedb449aa90
Tushar Chaudhary <tusharchaudhary692@gmail.com>
2021-03-14 13:52:41 +0530

    Hell Level Design

---
+++ TramFalling/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float MovementSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(0, 0, MovementSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(0, 0, -MovementSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ TramFalling/Assets/Scripts/allCollisionDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allCollisionDetector : MonoBehaviour
+{
+    [SerializeField] private GameObject trampolineTarget;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float movementSpeed = 5f;
+    private float counter = 0f;
+    private bool isGrounded = true;
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == trampolineTarget.tag)
+        {
+            toJump();
+        }
+        if(other.gameObject.tag == "floor")
+        {
+            isGrounded = true;
+        }
+
+    }
+    void toJump()
+    {
+        if(isGrounded == true)
+        {
+            rb.AddForce(new Vector3(0, movementSpeed + counter, 0), ForceMode.Impulse);
+            counter ++;
+            Debug.Log(counter);
+            isGrounded = false;
+        }
+    }
+}
